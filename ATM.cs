b23151a463ebc2ab4499a91c69152c1511403c14
Cael Church <cael.church@students.snow.edu>
2025-03-19 10:58:44 -0600

    ATM Class Framework

---
+++ ATM.cs
@@ -0,0 +1,47 @@
+public class ATM
+{
+    private bool cardInserted;
+    private bool pinValidated;
+    private BankServer bankServer;
+    private string currentCardNumber;
+
+    public void ATM(BankServer server)
+    {
+
+    }
+
+    public void insertCard(string cardNumber)
+    {
+
+    }
+
+    public bool enterPIN()
+    {
+
+    }
+
+    public void requestAmount()
+    {
+
+    }
+
+    public void dispenseCash()
+    {
+
+    }
+
+    public void ejectCard()
+    {
+
+    }
+
+    public void checkBalance()
+    {
+
+    }
+
+    public ATMAction(enum) getNextAction()
+    {
+        
+    }
+}
