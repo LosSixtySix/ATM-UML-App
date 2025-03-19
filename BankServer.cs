0937439ee3962de1f7ff9cf6b21add265bcb8a4b
kyler.olsen <kylerolsen17@gmail.com>
2025-03-19 10:51:16 -0600

    Implemented BankServer class

---
+++ BankServer.cs
@@ -0,0 +1,37 @@
+
+public class BankServer {
+
+	private Dictionary<string, (int pin, BankAccount account)> validCards;
+
+	public BankServer(Dictionary<string, (int pin, BankAccount account)> initialCards) {
+		validCards = initialCards;
+	}
+
+	public bool verifyCard(string cardNumber) {
+		return validCards.ContainsKey(cardNumber);
+	}
+
+	public bool verifyPin(string cardNumber, int pin) {
+		(int pin, BankAccount account) card;
+		if (validCards.TryGetValue(cardNumber, out card))
+			return card.pin == pin;
+		return false;
+	}
+
+	public bool processTransaction(string cardNumber, double amount) {
+		(int pin, BankAccount account) card;
+		if (validCards.TryGetValue(cardNumber, out card))
+			if (card.account.hasSufficientFunds(amount)) {
+				card.account.withdraw(amount);
+				return true;
+			}
+		return false;
+	}
+
+	public double checkBalance(string cardNumber) {
+		(int pin, BankAccount account) card;
+		if (validCards.TryGetValue(cardNumber, out card))
+			return card.account.getBalance();
+		return 0.0;
+	}
+}
